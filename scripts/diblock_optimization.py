a1a7e3ae207619dff3b3817d1f76a3aca0af69a4
Christian Tabedzki <cat159@scarletmail.rutgers.edu>
2022-06-17 17:20:01 -0400

    Sets up epochs and can run

--- scripts/diblock_optimization.py
+++ scripts/diblock_optimization.py
@@ -1,14 +1,22 @@
 #! env python
 
+from cmath import inf
 import cmaes
 import lammps_init_v2 as lmp
 import numpy as np
 from itertools import product
+import os
+import shutil
+import subprocess
+import math
 
+def myround(x, prec=2, base=.05):
+  return round(base * round(float(x)/base),prec)
 
 Dim = 3
 
-boxes = [ np.array([32.00, 32.00, 32.00])  ]
+resolution = 1
+boxes = [ np.array([16.00, 16.00, 16.00])  ]
 
 desired_rho0 = 5
 N = np.array([20]) # Total chain length; N_a = f_a * N
@@ -21,17 +29,19 @@ def topology_loss(frac, chiN):
 
 # TODO function to 
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+count = 0
+
 for n, box_dim in zip(N, boxes):
     print(f"n {n}")
 
     init_chiN = 5
     init_x = np.array([0.1, init_chiN])
-
     optimizer = cmaes.CMA(
         mean=init_x,
-        sigma=0.2,
-        bounds=np.array([[0.0, 0.5], [0.0, np.inf]]),
-        population_size=10)  # build CMA optimizer
+        sigma=1.0,
+        bounds=np.array([[0.025, 0.5], [0.0, np.inf]]),
+        population_size=4)  # build CMA optimizer
 
     # loop over generations
     for generation in range(optimization_generations):
@@ -39,29 +49,69 @@ for n, box_dim in zip(N, boxes):
         # calculate solutions for the current generation
         solutions = []
         for _ in range(optimizer.population_size):
+            os.chdir(dir_path)
             x = optimizer.ask()  # ask for parameters
 
+            x[0] = myround(x[0],prec=2, base=0.05)
             frac = x[0]
             chiN = x[1]
+
+            print(x)
+            # if math.isclose(frac,0.0) or math.isclose(frac,1.0):
+            #     solutions.append((x,inf))
+            #     continue
+
 
             box = lmp.input_config(box_dim[0],box_dim[1],box_dim[2])
             box.periodic = False
 
             box.add_diblock_rho0(1, 2, frac, n, desired_rho0, 1, 1)
 
+            Nx = int(box_dim[0] // resolution)
+            Ny = int(box_dim[1] // resolution)
+            Nz = int(box_dim[2] // resolution)
+
             config_file = f"conf_files/Nb_{n}" + (3*"_{}").format(box_dim[0],box_dim[1],box_dim[2]) \
                         + f"_diblock_f_{frac}_rho_{desired_rho0}.data"
             
             box.write(config_file) # Generates the initial configuration
 
-            command = f"./gpu-tild -in {input_file} -config {config_file}"
 
             # TODO build the command that will run the simulation
+            epoch_folder = f"epochs/{count}"
+            os.makedirs(epoch_folder, exist_ok=True)
+            input_file = "sample_input.gputild"
+            shutil.copy2(input_file, epoch_folder)
 
             loss = topology_loss(frac, chiN)  # TODO compute some loss based on topology
 
+            input_path = os.path.join(dir_path,epoch_folder,input_file)
+
+            kappa = 5
+            chi = chiN/n
+            kappa_chi = 2*kappa + chi
+
+            CONF_FOLDER="{CONF_DIR}"
+            INPUT_FILE="{INPUT_FILE}"
+            nx="{NX}"
+            ny="{NY}"
+            nz="{NZ}"
+            self_int="{POLY_SELF_AMP}"
+            cross_int="{POLY_CROSS_AMP}"
+            conf_folder = os.path.join(dir_path, "conf_files")
+            command=f"sed -i -e 's|{CONF_FOLDER}|{conf_folder}|g' -e 's|{INPUT_FILE}|{config_file}|'\
+                -e 's|{nx}|{Nx}|' -e 's|{ny}|{Ny}|' -e 's|{nz}|{Nz}|'\
+                    -e 's|{self_int}|{kappa}|' -e 's|{cross_int}|{kappa_chi}|'  {input_path}"
+            # print(command)
+            subprocess.call([command], shell=True)
+            os.chdir(epoch_folder)
+            command = f"echo 'gpu-tild -in {input_file}'"
+            subprocess.call([command], shell=True)
+
+            loss = topology_loss(frac,chiN)  # TODO compute some loss based on topology
+            count = count+1
             solutions.append((x, loss))  # append params & loss to solution list
-        
+            # print(solutions)
         optimizer.tell(solutions)  # update the optimizer with solutions
 
 
